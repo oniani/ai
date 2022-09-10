001b065fac12acc082ad03b1038cba1d016e3aa4
David Oniani <onianidavid@gmail.com>
2022-09-09 22:24:44 -0400

    K-Nearest Neighbors (k-NN)

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Artificial Intelligence (AI).
   - Gaussian Naive Bayes
     - [Implementation][gaussian_naive_bayes]
     - [Theory][gaussian_naive_bayes_theory]
+  - K-Nearest Neighbors (k-NN)
+    - [Implementation][k_nearest_neighbors]
+    - [Theory][k_nearest_neighbors_theory]
 
 ## License
 
@@ -16,3 +19,5 @@ Artificial Intelligence (AI).
 [license]: LICENSE
 [gaussian_naive_bayes]: models/gaussian_naive_bayes.py
 [gaussian_naive_bayes_theory]: https://en.wikipedia.org/wiki/Naive_Bayes_classifier#Gaussian_naive_Bayes
+[k_nearest_neighbors]: models/k_nearest_neighbors.py
+[k_nearest_neighbors_theory]: https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm
---
+++ models/k_nearest_neighbors.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from dataclasses import dataclass
+
+
+@dataclass
+class KNN:
+    features: np.ndarray
+    labels: np.ndarray
+    k: int
+
+    def predict(self, features: np.ndarray) -> np.ndarray:
+        """Performs inference on the given features."""
+
+        num_samples, _ = features.shape
+
+        predictions = np.empty(num_samples)
+        for idx, feature in enumerate(features):
+            distances = [np.linalg.norm(feature - train_feature) for train_feature in self.features]
+            k_sorted_idxs = np.argsort(distances)[: self.k]
+            most_common = np.bincount([self.labels[idx] for idx in k_sorted_idxs]).argmax()
+            predictions[idx] = most_common
+
+        return predictions
+
+
+if __name__ == "__main__":
+    from sklearn import datasets
+    from sklearn.metrics import precision_recall_fscore_support
+    from sklearn.model_selection import train_test_split
+
+    iris = datasets.load_iris()
+
+    train_features, test_features, train_labels, test_labels = train_test_split(
+        iris.data, iris.target, test_size=0.25, random_state=0  # type: ignore
+    )
+
+    knn = KNN(train_features, train_labels, k=3)  # type: ignore
+    predictions = knn.predict(test_features)  # type: ignore
+
+    accuracy = np.mean(predictions == test_labels)
+    precision, recall, fscore, _ = precision_recall_fscore_support(
+        test_labels, predictions, average="macro"
+    )
+
+    print(f"Accuracy:  {accuracy:.3f}")
+    print(f"Precision: {recall:.3f}")
+    print(f"Recall:    {precision:.3f}")
+    print(f"F-score:   {fscore:.3f}")
