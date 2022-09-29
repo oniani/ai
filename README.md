b4865ea63888fa3e9bff42ceafd0262c7eab6ced
David Oniani <onianidavid@gmail.com>
2022-09-29 11:33:23 -0400

    Leaky ReLU Activation Function

--- README.md
+++ README.md
@@ -16,6 +16,11 @@ _Artificial Intelligence (AI) - Contains Code for Several YouTube Series._
     - [:orange_book: Theory][relu_theory]
     - [:chart_with_upwards_trend: Plot][relu_plot]
     - [:tv: YouTube Video: Discussing and Implementing ReLU and Its Derivative Using PyTorch][relu_youtube]
+  - Leaky ReLU
+    - [:rocket: Implementation][leaky_relu]
+    - [:orange_book: Theory][leaky_relu_theory]
+    - [:chart_with_upwards_trend: Plot][leaky_relu_plot]
+    - [:tv: YouTube Video: Discussing and Implementing Leaky ReLU and Its Derivative Using PyTorch][leaky_relu_youtube]
 
 - **Machine Learning Models From Scratch Using NumPy**
 
@@ -53,6 +58,10 @@ _Artificial Intelligence (AI) - Contains Code for Several YouTube Series._
 [relu_theory]: https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
 [relu_plot]: activations/plots/relu.png
 [relu_youtube]: https://www.youtube.com/watch?v=93qjwrP7PfE
+[leaky_relu]: activations/leaky_relu.py
+[leaky_relu_theory]: https://en.wikipedia.org/wiki/Rectifier_(neural_networks)#Leaky_ReLU
+[leaky_relu_plot]: activations/plots/leaky_relu.png
+[leaky_relu_youtube]: https://www.youtube.com/watch?v=1HLKeWG0qnE
 [gaussian_naive_bayes]: models/gaussian_naive_bayes.py
 [gaussian_naive_bayes_theory]: https://en.wikipedia.org/wiki/Naive_Bayes_classifier#Gaussian_naive_Bayes
 [gaussian_naive_bayes_youtube]: https://www.youtube.com/watch?v=maJIRFeQBVI
---
+++ activations/leaky_relu.py
@@ -0,0 +1,65 @@
+import torch
+
+
+class LeakyReLU(torch.autograd.Function):
+    """The Leaky Rectified Linear Unit (Leaky ReLU) activation function."""
+
+    @staticmethod
+    def forward(ctx, data: torch.Tensor, alpha: float = 1e-2) -> torch.Tensor:
+        """Performs a forward pass."""
+
+        ctx.save_for_backward(data, torch.tensor(alpha).double())
+        return torch.where(data < 0.0, alpha * data, data)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:
+        """Performs a backpropagation."""
+
+        data, alpha = ctx.saved_tensors
+        grad = torch.where(data <= 0.0, alpha, 1.0)
+        return grad_output * grad
+
+
+# Alternative impl {{{
+#
+# Can your activation function be expressed as a combination of existing PyTorch functions?
+# If yes, no need to implement the `backward` method.
+#
+# import torch
+# import torch.nn as nn
+#
+#
+# class LeakyReLU(nn.Module):
+#     """The Rectified Linear Unit (ReLU) activation function."""
+#
+#     def __init__(self) -> None:
+#         """Inherits from `nn.Module`."""
+#
+#         super().__init__()
+#
+#     def forward(self, data: torch.Tensor, alpha: float = 1e-2) -> torch.Tensor:
+#         """Performs a forward pass."""
+#
+#         return torch.where(data < 0.0, alpha * data, data)
+#
+# }}}
+
+
+# Testing (gradcheck) {{{
+
+if __name__ == "__main__":
+    # Sets the manual seed for reproducible experiments
+    torch.manual_seed(0)
+
+    relu = LeakyReLU.apply
+    data = torch.randn(4, dtype=torch.double, requires_grad=True)
+
+    # `torch.autograd.gradcheck` takes a tuple of tensors as input, check if your gradient evaluated
+    # with these tensors are close enough to numerical approximations and returns `True` if they all
+    # verify this condition.
+    if torch.autograd.gradcheck(relu, data, eps=1e-8, atol=1e-7):  # type: ignore
+        print("gradcheck successful")
+    else:
+        print("gradcheck unsuccessful")
+
+# }}}
